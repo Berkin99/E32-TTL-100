c70c20770ac97f0152da4984cd9e761078545d71
Berkin Yılmaz <111027197+Berkin99@users.noreply.github.com>
2024-10-02 04:38:21 +0300

    tabfix

--- e32100.h
+++ e32100.h
@@ -9,10 +9,10 @@
  *  UART communication.
  *  Changeable all possible settings.
  *
- *	Updates and bug reports :  @ https://github.com/Berkin99/E32-TTL-100
+ *  Updates and bug reports :  @ https://github.com/Berkin99/E32-TTL-100
  *
  *  22.12.2023 : Created E32-TTL-100 module driver.
- *	12.01.2024 : AUX Pin taken into account.
+ *  12.01.2024 : AUX Pin taken into account.
  *  14.01.2024 : UART Receive DMA abort (bugfix).
  *  22.01.2024 : DMA is optional.
  *  02.09.2024 : Object System
@@ -28,136 +28,136 @@
 
 #include <stdint.h>
 
-#define E32100_ERROR 	1
-#define E32100_OK	 	0
+#define E32100_ERROR    1
+#define E32100_OK       0
 
-#define E32100_MAX_BUFFER_LEN 			58
-#define E32100_MODE_CHANGE_INTERVAL 	25
-#define E32100_AUX_CHANGE_INTERVAL 		2
-#define E32100_COMMAND_INTERVAL			5
-#define E32100_TIMEOUT					100
+#define E32100_MAX_BUFFER_LEN         58
+#define E32100_MODE_CHANGE_INTERVAL   25
+#define E32100_AUX_CHANGE_INTERVAL    2
+#define E32100_COMMAND_INTERVAL       5
+#define E32100_TIMEOUT                100
 
-#define E32100_DEFAULT_CHANNEL		 	433
-#define E32100_DEFAULT_ADDH				0x00
-#define E32100_DEFAULT_ADDL				0x00
+#define E32100_DEFAULT_CHANNEL        433
+#define E32100_DEFAULT_ADDH           0x00
+#define E32100_DEFAULT_ADDL           0x00
 
-#define E32100_DEFAULT_AIRDATARATE		0x05
-#define E32100_DEFAULT_BAUDRATE			0x03
-#define E32100_DEFAULT_PARITY			0x00
+#define E32100_DEFAULT_AIRDATARATE    0x05
+#define E32100_DEFAULT_BAUDRATE       0x03
+#define E32100_DEFAULT_PARITY         0x00
 
-#define E32100_DEFAULT_TXPOWER			0x00
-#define E32100_DEFAULT_FEC				0x01
-#define E32100_DEFAULT_WAKEUP			0x00
-#define E32100_DEFAULT_IOMODE			0x01
-#define E32100_DEFAULT_TXMODE			0x00
+#define E32100_DEFAULT_TXPOWER        0x00
+#define E32100_DEFAULT_FEC            0x01
+#define E32100_DEFAULT_WAKEUP         0x00
+#define E32100_DEFAULT_IOMODE         0x01
+#define E32100_DEFAULT_TXMODE         0x00
 
-#define E32100_OFFSET_AIRDATARATE		0
-#define E32100_OFFSET_BAUDRATE			3
-#define E32100_OFFSET_PARITY			6
+#define E32100_OFFSET_AIRDATARATE     0
+#define E32100_OFFSET_BAUDRATE        3
+#define E32100_OFFSET_PARITY          6
 
-#define E32100_OFFSET_TXPOWER			0
-#define E32100_OFFSET_FEC				2
-#define E32100_OFFSET_WAKEUP			3
-#define E32100_OFFSET_IOMODE			6
-#define E32100_OFFSET_TXMODE			7
+#define E32100_OFFSET_TXPOWER         0
+#define E32100_OFFSET_FEC             2
+#define E32100_OFFSET_WAKEUP          3
+#define E32100_OFFSET_IOMODE          6
+#define E32100_OFFSET_TXMODE          7
 
 typedef enum {
-	E32100_MODE_NORMAL = 0,
-	E32100_MODE_WAKEUP,
-	E32100_MODE_POWERSAVE,
-	E32100_MODE_SLEEP
+    E32100_MODE_NORMAL = 0,
+    E32100_MODE_WAKEUP,
+    E32100_MODE_POWERSAVE,
+    E32100_MODE_SLEEP
 }E32100_Mode_e;
 
 /*  In sleep mode（mode 3：M1=1, M0=1）, it supports below instructions on list.
- *	Only support 9600 and 8N1 format when set
+ *    Only support 9600 and 8N1 format when set
  */
 typedef enum {
-	E32100_CMD_SAVE_ON  	= 0xC0,
-	E32100_CMD_READ_CFG 	= 0xC1,
-	E32100_CMD_SAVE_OFF		= 0xC2,
-	E32100_CMD_MODULE   	= 0xC3,
-	E32100_CMD_RESET     	= 0xC4
+    E32100_CMD_SAVE_ON      = 0xC0,
+    E32100_CMD_READ_CFG     = 0xC1,
+    E32100_CMD_SAVE_OFF        = 0xC2,
+    E32100_CMD_MODULE       = 0xC3,
+    E32100_CMD_RESET         = 0xC4
 }E32100_Command_e;
 
 typedef enum {
-	E32100_ADR_300 = 0,
-	E32100_ADR_1200,
-	E32100_ADR_2400,
-	E32100_ADR_4800,
-	E32100_ADR_9600,
-	E32100_ADR_19200
+    E32100_ADR_300 = 0,
+    E32100_ADR_1200,
+    E32100_ADR_2400,
+    E32100_ADR_4800,
+    E32100_ADR_9600,
+    E32100_ADR_19200
 }E32100_AirDataRate_e;
 
 typedef enum {
-	E32100_BAUDRATE_1200 = 0,
-	E32100_BAUDRATE_2400,
-	E32100_BAUDRATE_4800,
-	E32100_BAUDRATE_9600,
-	E32100_BAUDRATE_19200,
-	E32100_BAUDRATE_38400,
-	E32100_BAUDRATE_57600,
-	E32100_BAUDRATE_115200
+    E32100_BAUDRATE_1200 = 0,
+    E32100_BAUDRATE_2400,
+    E32100_BAUDRATE_4800,
+    E32100_BAUDRATE_9600,
+    E32100_BAUDRATE_19200,
+    E32100_BAUDRATE_38400,
+    E32100_BAUDRATE_57600,
+    E32100_BAUDRATE_115200
 }E32100_BaudRate_e;
 
 typedef enum {
-	E32100_PARITY_8N1 = 0,
-	E32100_PARITY_8O1,
-	E32100_PARITY_8E1
+    E32100_PARITY_8N1 = 0,
+    E32100_PARITY_8O1,
+    E32100_PARITY_8E1
 }E32100_UartParity_e;
 
 typedef enum {
-	E32100_TXPOWER_20 = 0,
-	E32100_TXPOWER_17,
-	E32100_TXPOWER_14,
-	E32100_TXPOWER_10
+    E32100_TXPOWER_20 = 0,
+    E32100_TXPOWER_17,
+    E32100_TXPOWER_14,
+    E32100_TXPOWER_10
 }E32100_TxPower_e;
 
 typedef enum {
-	E32100_FEC_OFF = 0,
-	E32100_FEC_ON
+    E32100_FEC_OFF = 0,
+    E32100_FEC_ON
 }E32100_Fec_e;
 
 typedef enum {
-	E32100_WAKEUP_250 = 0,
-	E32100_WAKEUP_500,
-	E32100_WAKEUP_750,
-	E32100_WAKEUP_1000,
-	E32100_WAKEUP_1250,
-	E32100_WAKEUP_1500,
-	E32100_WAKEUP_1750,
-	E32100_WAKEUP_2000
+    E32100_WAKEUP_250 = 0,
+    E32100_WAKEUP_500,
+    E32100_WAKEUP_750,
+    E32100_WAKEUP_1000,
+    E32100_WAKEUP_1250,
+    E32100_WAKEUP_1500,
+    E32100_WAKEUP_1750,
+    E32100_WAKEUP_2000
 }E32100_WakeUpTime_e;
 
 typedef enum {
-	E32100_IOMODE_OFF = 0,
-	E32100_IOMODE_ON
+    E32100_IOMODE_OFF = 0,
+    E32100_IOMODE_ON
 }E32100_IOMode_e;
 
 typedef enum {
-	E32100_TXMODE_TRANSPARENT = 0,
-	E32100_TXMODE_FIXED
+    E32100_TXMODE_TRANSPARENT = 0,
+    E32100_TXMODE_FIXED
 }E32100_TXMode_e;
 
 typedef struct E32100_Sped_s{
-	E32100_AirDataRate_e airDataRate;
-	E32100_BaudRate_e baudRate;
-	E32100_UartParity_e parity;
+    E32100_AirDataRate_e airDataRate;
+    E32100_BaudRate_e baudRate;
+    E32100_UartParity_e parity;
 }E32100_Sped_t;
 
 typedef struct E32100_Option_s{
-	E32100_TxPower_e txPower;
-	E32100_Fec_e fec;
-	E32100_WakeUpTime_e wakeUpTime;
-	E32100_IOMode_e ioMode;
-	E32100_TXMode_e txMode;
+    E32100_TxPower_e txPower;
+    E32100_Fec_e fec;
+    E32100_WakeUpTime_e wakeUpTime;
+    E32100_IOMode_e ioMode;
+    E32100_TXMode_e txMode;
 }E32100_Option_t;
 
 typedef struct E32100_Config_s{
-	uint8_t 		addh;
-	uint8_t 		addl;
-	E32100_Sped_t  	sped;
-	uint16_t  		channel;
-	E32100_Option_t option;
+    uint8_t         addh;
+    uint8_t         addl;
+    E32100_Sped_t      sped;
+    uint16_t          channel;
+    E32100_Option_t option;
 }E32100_Config_t;
 
 /* Read via UART Function Pointer */
@@ -177,13 +177,13 @@ typedef int8_t (*E32100_PinGet_t)(void* pin);
 
 /* E32100 Object */
 typedef struct E32100_Handle_s{
-	E32100_Mode_e mode;
-	void* pIntf;
-	void* M0;
-	void* M1;
-	void* AUX;
-	E32100_PinSet_t pinSet;
-	E32100_PinGet_t pinGet;
+    E32100_Mode_e mode;
+    void* pIntf;
+    void* M0;
+    void* M1;
+    void* AUX;
+    E32100_PinSet_t pinSet;
+    E32100_PinGet_t pinGet;
     E32100_Read_t   read;
     E32100_Write_t  write;
     E32100_Delay_t  delay;
@@ -204,7 +204,7 @@ typedef struct E32100_Handle_s{
  *  @return A new E32100_Handle_t structure initialized with the provided parameters.
  */
 E32100_Handle_t E32100_NewHandle(void* pIntf, void* pinM0, void* pinM1, void* pinAUX, 
-	E32100_PinSet_t setf, E32100_Read_t readf, E32100_Write_t writef, E32100_Delay_t delayf);
+    E32100_PinSet_t setf, E32100_Read_t readf, E32100_Write_t writef, E32100_Delay_t delayf);
 
 /*
  *  @brief This API initializes the E32100 module by setting it to normal mode.
